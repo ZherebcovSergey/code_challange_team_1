e6f1617aa04f05a96dd9a717eb81445ddd50614b
Oleksandr Diudiun <a.diudiun@gmail.com>
2017-07-15 14:24:40 +0300

    f

--- 1.php
+++ 1.php
@@ -1,1 +1,58 @@
 <?php
+
+function solution($S) {
+
+	if (empty($S) || $S == "") {
+		return true;
+	}
+
+	$brackets = str_split($S);
+	$count = count($brackets);
+	if ($count % 2 > 0) {
+		return false;
+	}
+
+	$open = array();
+	$close = array(
+		"{" => "}",
+		"[" => "]",
+		"(" => ")",
+	);
+	for ($i = 0; $i < $count; $i++) {
+		if ($brackets[$i] == "{" || $brackets[$i] == "[" || $brackets[$i] == "(") {
+			array_push($open, $brackets[$i]);
+		} else {
+			if (empty($open)) {
+				return false;
+			}
+
+			if ($close[end($open)] == $brackets[$i]) {
+				array_pop($open);
+			} else {
+				return false;
+			}
+
+		}
+	}
+	if (empty($open)) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+$arrS = array(
+	"",
+	"{}",
+	"()",
+	"[]",
+	"{[()()]}",
+	"([)()]",
+	"((){])[]",
+);
+
+foreach ($arrS as $key => $value) {
+	$res = solution($value);
+	echo $key . " => " . (int) $res . "\n";
+}
