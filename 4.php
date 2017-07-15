de1ed19e2136358f3cccf2f0476c64705632d815
Igor Suhov <ukrSuhovIS@gmail.com>
2017-07-15 17:25:33 +0300

    upgrade

--- 4.php
+++ 4.php
@@ -1,1 +1,41 @@
 <?php
+
+function solution($H) {
+
+	$stack = array();
+	$total = 0;
+	foreach ($H as $height) {
+		if(empty($stack)) {
+			array_push($stack, $height);
+			$total++;
+		}
+		else{
+			while (!empty($stack)) {
+				$tmpHeight = end($stack);
+				if ($tmpHeight < $height) {
+					array_push($stack, $height);
+					$total++;
+					break;
+				} elseif($tmpHeight > $height) {
+					array_pop($stack);
+				}
+				else break;
+			}
+			if (empty($stack)) {
+				array_push($stack, $height);
+				$total++;
+			}
+		}
+	}
+	return $total;
+}
+$Harr = array(
+	0=>array(8,8,5,7,9,8,7,4,8)
+);
+foreach ($Harr as $key => $value) {
+	$res = solution($value);
+	echo $key . " => " . (int) $res . "\n";
+}
+
+###
+###
